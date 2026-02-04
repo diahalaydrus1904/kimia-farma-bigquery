ae5f0eb772c848d29a14a355bd32832cd353dfe4
Rodiah Hasan Alaydrus <diahalaydrus1904@gmail.com>
2026-02-04 23:10:59 +0700

    Add files via upload
    Add analysis SQL

---
+++ create_analysis_table.sql
@@ -0,0 +1,44 @@
+CREATE OR REPLACE TABLE kimia_farma.kf_analysis AS
+SELECT
+  t.transaction_id,
+  t.date,
+  c.branch_id,
+  c.branch_name,
+  c.kota,
+  c.provinsi,
+  c.rating AS rating_cabang,
+  t.customer_name,
+  p.product_id,
+  p.product_name,
+  p.price AS actual_price,
+  t.discount_percentage,
+
+  -- Persentase gross laba
+  CASE
+    WHEN p.price <= 50000 THEN 0.10
+    WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
+    WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
+    WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+    ELSE 0.30
+  END AS persentase_gross_laba,
+
+  -- Nett sales (harga setelah diskon)
+  p.price * (1 - t.discount_percentage) AS nett_sales,
+
+  -- Nett profit
+  (p.price * (1 - t.discount_percentage)) *
+  CASE
+    WHEN p.price <= 50000 THEN 0.10
+    WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
+    WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
+    WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+    ELSE 0.30
+  END AS nett_profit,
+
+  t.rating AS rating_transaksi
+
+FROM kimia_farma.kf_final_transaction t
+JOIN kimia_farma.kf_product p
+  ON t.product_id = p.product_id
+JOIN kimia_farma.kf_kantor_cabang c
+  ON t.branch_id = c.branch_id;
